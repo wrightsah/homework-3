58cf28c8720a83b6e278617d3c38bdcf96858cfd
Stephen Wright <wrightsah@icloud.com>
2020-12-14 15:01:16 -0500

    created invdividual character loops

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
 //Define variables 
 
 var passwordLength
@@ -33,6 +30,13 @@ var lowercaseInput
 var uppercaseInput
 var numericCharInput
 var specialCharInput
+
+// variables for generated characters
+
+var lowercaseCharacter
+var uppercaseCharacter
+var numericCharacter
+var specialCharacter
 
 // random number for loop
 
@@ -52,22 +56,36 @@ For (var i = 0; i < passwordLength; i++) {
 	// run random number generator
 	randomNumberGenerator()
 
-	// if random number is 0, create upper case
+	// if random number is 0, get upper case character from array
 
 	if (randomNumber === 0) {
-		uppercaseInput = Math.floor(Math.random() * uppercaseArray.length)
-		
+		uppercaseCharacter = uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)];
+		// test if working
+		console.log("Generated UC =" + uppercaseCharacter)
 	}
 	
-	
-	// if random number is 1, create upper case
-	
-	
-	// if random number is 2, create number
-	
-	
-	// if random number is 3, create special character
+	// if random number is 1, get lower case character from array
+		else if (randomNumber === 1) {
+			lowercaseCharacter = lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)];
+			// test if working
+			console.log("Generated LC =" + lowercaseCharacter)
 
+		}
+	 
+	
+	// if random number is 2, get numeric character from array
+		else if (randomNumber === 2) {
+			numericCharacter = numericCharArray[Math.floor(Math.random() * numericCharArray.length)];
+			// test if working
+			console.log("Generated NC =" + numericCharacter)
+		}
+	
+	// if random number is 3, get special character from array
+		else if (randomNumber === 3) {
+			specialCharacter = specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
+			// test if working
+			console.log("Generated SC =" + specialCharacter)
+		}
 	
 	// add selected character to passwordoutput string
 
