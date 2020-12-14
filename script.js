bedb9727e0eabf500c55529bc3f60dc7119257ef
Stephen Wright <wrightsah@icloud.com>
2020-12-14 14:14:18 -0500

    fixed special character array

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ var passwordLength
 
 //Define arrays for data
 
-uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-numericCharArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
-specialCharArray = 
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numericCharArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+var specialCharArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 //Store input from buttons
 
