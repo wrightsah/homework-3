81af587712d36abf2dd5666538abe41317e2ed21
Stephen Wright <wrightsah@icloud.com>
2020-12-16 14:08:06 -0500

    updated README sections

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# homework-3
-HW3: Password Generator
+# HW3: Password Generator
+
+## The Project
+
+[View Application Here](wrightsah.github.io/homework-3)
+
+This site was built to generate a secure password that incorporates uppercase characters, lowercase characters, numeric characters, and special characters. 
+
+### Instructions: 
+
+The user will select the password length (between 8 and 128 characters) and 
+
+## Requirements
+
+This website requires a web browser such as Safari, Firefox, Chrome, or Opera to view and run. 
+
+<!-- insert screenshots of prompts and finished password -->
+
+## Credits
+
+HTML & CSS layouts were provided by UNC Coding Bootcamp. 
+
+## Contact
+
+You may contact me at wrightsah@icloud.com
+
+## License
+
+This project uses the following license: [MIT License](License)
--- script.js
+++ script.js
@@ -7,145 +7,222 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Define variables 
+// Define variables
 
 //Define arrays for data
 
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numericCharacterArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
-var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var uppercaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowercaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numericCharacterArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacterArray = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
 
 // Function to prompt for password parameters
 
 function getPasswordParameters() {
-	var passwordLength = parseInt(
-		prompt("How many characters?")
-	);
+  var passwordLength = parseInt(prompt("How many characters?"));
 
-	// confirm that passwordLength is a number
+  // confirm that passwordLength is a number
 
-	if (isNaN(passwordLength) === true ) {
-		alert("Must be a number.");
-		return;
-	}
+  if (isNaN(passwordLength) === true) {
+    alert("Must be a number.");
+    return;
+  }
 
-	// Set boundaries to ensure character length is between 8 - 128
+  // Set boundaries to ensure character length is between 8 - 128
 
-	if (passwordLength < 8) {
-		alert("Password must be at least 8 characters.")
-		return;
-	}
+  if (passwordLength < 8) {
+    alert("Password must be at least 8 characters.");
+    return;
+  }
 
-	if (passwordLength > 128) {
-		alert("Password may not be more than 128 characters.")
-		return;
-	}
+  if (passwordLength > 128) {
+    alert("Password may not be more than 128 characters.");
+    return;
+  }
 
-	//Store password parameters from prompts
+  //Store password parameters from prompts
 
-	var includeLowercase = confirm(
-		"Click OK to include lowercase characters."
-	);
+  var includeLowercase = confirm("Click OK to include lowercase characters.");
 
-	var includeUppercase = confirm(
-		"Click OK to include uppercase characters."
-	);
-	var includeNumericCharacter = confirm(
-		"Click OK to include numeric characters."
-	);
-	var includeSpecialCharacter = confirm(
-		"Click OK to include special characters."
-	);
+  var includeUppercase = confirm("Click OK to include uppercase characters.");
+  var includeNumericCharacter = confirm(
+    "Click OK to include numeric characters."
+  );
+  var includeSpecialCharacter = confirm(
+    "Click OK to include special characters."
+  );
 
-	// Ensure input contains at least one necessary parameter
+  // Ensure input contains at least one necessary parameter
 
-	if (
-		includeLowercase === false &&
-		includeUppercase === false && 
-		includeNumericCharacter === false &&
-		includeSpecialCharacter === false
-	) {
-		alert("Must choose at least one parameter.");
-		return;
-	}
+  if (
+    includeLowercase === false &&
+    includeUppercase === false &&
+    includeNumericCharacter === false &&
+    includeSpecialCharacter === false
+  ) {
+    alert("Must choose at least one parameter.");
+    return;
+  }
 
-	// Store selected parameters in object
+  // Store selected parameters in object
 
-	var passwordParameters = {
-		passwordLength: passwordLength,
-		includeLowercase: includeLowercase,
-		includeUppercase: includeUppercase,
-		includeNumericCharacter: includeNumericCharacter,
-		includeSpecialCharacter: includeSpecialCharacter,
-	};
+  var passwordParameters = {
+    passwordLength: passwordLength,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+    includeNumericCharacter: includeNumericCharacter,
+    includeSpecialCharacter: includeSpecialCharacter,
+  };
 
-	console.log(passwordParameters);
-	return passwordParameters;
+  console.log(passwordParameters);
+  return passwordParameters;
 }
 
 // Function - Get random element from an array
 function getRandom(arr) {
-	var randIndex = Math.floor(Math.random() * arr.length);
-	var randElement = arr[randIndex];
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
 
-	console.log(randElement);
-	return randElement;
+  console.log(randElement);
+  return randElement;
 }
 
 // Function = Generate password when button is clicked
 
 function generatePassword() {
-	var parameters = getPasswordParameters();
+  var parameters = getPasswordParameters();
 
-	var workingPassword = [];
+  var workingPassword = [];
 
-	var possibleParameters = [];
+  var possibleParameters = [];
 
-	var requiredParameters = [];
+  var requiredParameters = [];
 
-	// determine which characters are going to be part of generated password and generate the character
+  // determine which characters are going to be part of generated password and generate the character
 
-	if (parameters.includeLowercase) {
-		possibleParameters = possibleParameters.concat(lowercaseArray);
-		requiredParameters.push(getRandom(lowercaseArray));
-	}
+  if (parameters.includeLowercase) {
+    possibleParameters = possibleParameters.concat(lowercaseArray);
+    requiredParameters.push(getRandom(lowercaseArray));
+  }
 
-	if (parameters.includeUppercase) {
-		possibleParameters = possibleParameters.concat(uppercaseArray);
-		requiredParameters.push(getRandom(uppercaseArray));
-	}
+  if (parameters.includeUppercase) {
+    possibleParameters = possibleParameters.concat(uppercaseArray);
+    requiredParameters.push(getRandom(uppercaseArray));
+  }
 
-	if (parameters.includeNumericCharacter) {
-		possibleParameters = possibleParameters.concat(numericCharacterArray);
-		requiredParameters.push(getRandom(numericCharacterArray));
-	}
+  if (parameters.includeNumericCharacter) {
+    possibleParameters = possibleParameters.concat(numericCharacterArray);
+    requiredParameters.push(getRandom(numericCharacterArray));
+  }
 
-	if (parameters.includeSpecialCharacter) {
-		possibleParameters = possibleParameters.concat(specialCharacterArray);
-		requiredParameters.push(getRandom(specialCharacterArray));
-	}
-	
-	// for loop 1 - Ensure password runs for desired length
-	for (var i = 0; i < parameters.passwordLength; i++) {
-		var possibleParameter = getRandom(possibleParameters);
+  if (parameters.includeSpecialCharacter) {
+    possibleParameters = possibleParameters.concat(specialCharacterArray);
+    requiredParameters.push(getRandom(specialCharacterArray));
+  }
 
-		workingPassword.push(possibleParameter);
-	}
-	
-	// for loop 2 - Ensure password runs until each required character type is included
-	for (var i = 0; i < requiredParameters.length; i++) {
-		workingPassword[i] = requiredParameters[i];
-	}
+  // for loop 1 - Ensure password runs for desired length
+  for (var i = 0; i < parameters.passwordLength; i++) {
+    var possibleParameter = getRandom(possibleParameters);
 
-	// Convert workingPassword into a string and generatePassword
+    workingPassword.push(possibleParameter);
+  }
 
-	return workingPassword.join("");
+  // for loop 2 - Ensure password runs until each required character type is included
+  for (var i = 0; i < requiredParameters.length; i++) {
+    workingPassword[i] = requiredParameters[i];
+  }
+
+  // Convert workingPassword into a string and generatePassword
+
+  return workingPassword.join("");
 }
-
